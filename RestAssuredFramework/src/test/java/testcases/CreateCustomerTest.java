5b4de9c0cb9bd506f37f7dbce7019b93d883ae02
Valmiki Vishwakarma <you@example.com>
2022-11-18 23:02:05 +0530

    Committing getCustomerAPIWithValidSecretKey.

--- RestAssuredFramework/src/test/java/testcases/CreateCustomerTest.java
+++ RestAssuredFramework/src/test/java/testcases/CreateCustomerTest.java
@@ -1,5 +1,23 @@
 package testcases;
 
+import static io.restassured.RestAssured.given;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
 public class CreateCustomerTest {
+
+	@Test
+	public void getCustomerAPIWithValidSecretKey() {
+		
+		RestAssured.baseURI="https://api.stripe.com/";
+		Response response=given().auth().basic("sk_test_4eC39HqLyjWDarjtT1zdp7dc", "")
+		.get("v1/customers");
+
+		response.prettyPrint();
+		System.out.println(response.getStatusCode());
+	}
 
 }
